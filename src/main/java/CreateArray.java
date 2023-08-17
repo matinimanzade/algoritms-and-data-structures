1c8aa2ad049fc19057b5166552cf8b0ed5b7ad7c
Matin Imanzade <matinimanzade@gmail.com>
2023-08-17 10:19:26 +0400

    CreateArray added

---
+++ src/main/java/CreateArray.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class CreateArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int length = scanner.nextInt();
+
+        int[] arr = createArray(length);
+        System.out.println(arr);
+    }
+
+    public static int[] createArray(int length){
+
+        return null;
+    }
+}
