5f6847bc31b989181adaa5b2421cacd9e8c2f9d7
Matin Imanzade <matinimanzade@gmail.com>
2023-08-17 10:38:16 +0400

    last changed add to github

--- src/main/java/CreateArray.java
+++ src/main/java/CreateArray.java
@@ -1,16 +1,29 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ * Istifadeci evvelce massivin uzunlugunu teyin edir
+ * Daha sonra Scanner vasitesiyle ora ededler daxil edir
+ */
 public class CreateArray {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Massiv uzunlugu ne qeder olsun?");
         int length = scanner.nextInt();
-
+        System.out.println("------------------");
+        System.out.println("Uzunlugu " + length + " olan massiv yaradilacaq");
         int[] arr = createArray(length);
-        System.out.println(arr);
+        System.out.println("Uzunlugu " + length + " olan massiv: " + Arrays.toString(arr));
+
     }
 
-    public static int[] createArray(int length){
-
-        return null;
+    public static int[] createArray(int length) {
+        int[] arr = new int[length];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ededleri massive daxil edin : Daxil ede bileceyiniz maksimum eded sayi => " + length);
+        for (int i = 0; i < length; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        return arr;
     }
 }
--- src/main/java/FlattenFiguresFromTheTopToTheBottom.java
+++ src/main/java/FlattenFiguresFromTheTopToTheBottom.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Sort {
+public class FlattenFiguresFromTheTopToTheBottom {
     public static void main(String[] args) {
         /**
          *  12345
